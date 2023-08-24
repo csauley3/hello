edf326b82f1750222e5592b67610bf8ad0e20005
Collin Sauley <csauley3@huskers.unl.edu>
2023-08-24 12:39:30 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Collin Sauley
+ * Date: 2023/8/24
+ *
+ * Printing my name in C
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
